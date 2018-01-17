a3a89a6560a7c9101346468fa2769b05be0d4cfe
Julia Lidstrom <juleslidstrom@gmail.com>
2018-01-17 16:46:17 -0600

    Final files

--- README.md
+++ README.md
@@ -16,10 +16,12 @@ Create a train schedule application that incorporates Firebase to host arrival a
 * I primarily used Bootstrap Cerulean theme for styling. I added minimal CSS to customize some of the components. 
 
 ## Challenges
-* I couldn't implement an elegant solution for preventing form submission if any fields were left blank. I added logic to check that the value of each field is greater than 0.
+* I couldn't implement an elegant solution for preventing form submission if any fields were left blank. I added logic to check that the value of each field is greater than 0. 
 * Moment.js library is useful but difficult to understand. I had to experiment a lot until I found the right methods for needed calculations.
+* Removing a child node in the Firebase database.
 
 ## Feedback and Questions
+* I was able to capture a child node key after pushing it to database. However, I still couldn't delete the child node on click. I would really appreciate your help with soling it!
 * What's the best method to validate a form?
 * I welcome overall code structure feedback.
 
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,6 +11,8 @@ messagingSenderId: "1061301952271"
 firebase.initializeApp(config);
 
 var database = firebase.database();
+//The following var is for my attempt to  remove the child node in Firebase on click
+var databaseChildKey = "";
 
 //On click funciton to capture user input 
 
@@ -23,14 +25,24 @@ $("#addTrain").on("click",function(event){
 	var firstTrain = $("#train-time").val().trim();
 	var frequency = $("#train-frequency").val().trim();
 
-	if (trainName.length && destination.length && firstTrain.length && frequency.length >0){
+	
+	//Validate that all fields are filled out (not a validation of whether the values are in accepted format)	
+	// if (trainName.length && destination.length && firstTrain.length && frequency.length >0){
+	if (trainName && destination && firstTrain && frequency !=""){
 	//Push values to the Firebase database
-		database.ref().push({
+		var newChildRef=database.ref().push({
 			trainName:trainName,
 			destination:destination,
 			firstTrain:firstTrain,
 			frequency:frequency,
 		});
+
+			var newNodeID = newChildRef.getKey();
+			var newItem = {
+	            id:newNodeID
+	        };
+	        newChildRef.update(newItem);
+	
 
 		$("#train-name").val("");
 		$("#train-destination").val("");
@@ -45,8 +57,12 @@ $("#addTrain").on("click",function(event){
 	//Add event listener for every new child created in Firebase database
 database.ref().on("child_added",function(snapshot){
 
+	//Get the unique key for the child
+	databaseChildKey = snapshot.key;
+	console.log(databaseChildKey);
 	//Assign database values to variables
 	var sv = snapshot.val();
+	
 	var trainName = sv.trainName;
 	var destination = sv.destination;
 	var firstTrain = sv.firstTrain;
@@ -58,11 +74,27 @@ database.ref().on("child_added",function(snapshot){
 	var minutesRemaining = frequency-(timeDiff%frequency);
 	var nextTrain = moment().add(minutesRemaining,"minutes");
 	var nextTrainDisplay = moment(nextTrain).format("hh:mm a");
+
+	var deleteTrain = $("<button>"); 
+	$('#table tbody tr').append('<td><a href="#" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a><a href="#" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Delete </a></td>');
+	// var deleteTrain = $("<button>").text("Delete"); 
 	
 	//Populate a new row for every train in the database
-	$("#train-input > tbody").prepend ("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +  frequency + "</td><td>" + nextTrainDisplay + "</td><td>" + minutesRemaining + ' minutes'+ "</td></tr>");
+	$("#train-input > tbody").prepend ("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +  frequency + "</td><td>" + nextTrainDisplay + "</td><td>" + minutesRemaining + ' minutes'+ "</td><td>" + "<button type='button' id ='remove' class='btn btn-primary disabled'><i class='fa fa-trash-o'></i></a>" + "</td></tr>");
 }, function(errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
 
 
+//My attempt to remove the table row AND associated child node in the database. I was successfull with the former bubt not the latter
+ $(document).on("click", "#remove",function(event){
+ 		console.log(event)
+	  	event.preventDefault();
+	 	
+	 	//This is my attempt to pass the key to the click event to remove the Firebase node
+	 	// var key = $("#remove").attr(databaseChildKey);	
+	 	// console.log(key);
+	 	// database.child(key).remove(); 	
+				  
+	 	$(this).closest("tr").remove();
+ });
